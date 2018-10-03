d3651ceccf96c591fc30684f74cb4c3012f19c0a
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2018-10-03 17:29:39 -0300

    navbar faltando o css

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Code from './Code';
+import NavbarApp from './NavbarApp';
 class App extends Component {
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <NavbarApp /> Funciona.
+          </p>
+
+          <p>
+            Edit <Code plus="*">src/App.js</Code> and save to reload.
           </p>
           <a
             className="App-link"
